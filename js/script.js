d02002a49a4c53a39dd70c18b2aa9a7c9df3a521
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2022-08-14 21:13:44 +0100

    done with the functions for mobile view

--- js/script.js
+++ js/script.js
@@ -1,20 +1,30 @@
+//variables to enable hidden parts show
 const navIcon = document.querySelector(".nav__icon");
 const navigation = document.querySelector(".nav");
 const closeIcon = document.querySelector(".close");
 const cartIcon = document.querySelector(".cart-icon");
 const cartDisplay = document.querySelector(".cart-display");
+const overlay = document.querySelector(".overlay");
+
+//variables to make the images scroll
 var images = [
   "../images/image-product-1.jpg",
   "../images/image-product-2.jpg",
   "../images/image-product-3.jpg",
   "../images/image-product-4.jpg",
 ];
-var currentIndex = 0;
+let currentIndex = 0;
 
-var currentImg = document.querySelector(".main-img");
-var next = document.querySelector(".next");
-var prev = document.querySelector(".previous");
+let currentImg = document.querySelector(".main-img");
+let next = document.querySelector(".next");
+let prev = document.querySelector(".previous");
 
+//variables for incrementing and decrementing number of items
+const plus = document.querySelector(".add");
+const minus = document.querySelector(".minus");
+const numberBox = document.querySelector(".number");
+
+//toggling functions
 const toggle = function (elem) {
   if (elem.style.visibility === "visible") {
     elem.style.visibility = "hidden";
@@ -23,7 +33,16 @@ const toggle = function (elem) {
   }
 };
 
-var incrementIndex = function () {
+const setDisplay = function (elem) {
+  if (elem.style.display === "flex") {
+    elem.style.display = "none";
+  } else {
+    elem.style.visibility = "flex";
+  }
+};
+
+//these make the pictures change
+let incrementIndex = function () {
   currentIndex = currentIndex + 1;
   if (currentIndex > images.length - 1) {
     currentIndex = 0;
@@ -31,19 +50,13 @@ var incrementIndex = function () {
   return currentIndex;
 };
 
-var decrementIndex = function () {
+let decrementIndex = function () {
   currentIndex = currentIndex - 1;
   if (currentIndex < 0) {
     currentIndex = images.length - 1;
   }
   return currentIndex;
 };
-
-navIcon.addEventListener("click", () => toggle(navigation));
-
-closeIcon.addEventListener("click", () => toggle(navigation));
-
-cartIcon.addEventListener("click", () => toggle(cartDisplay));
 
 next.addEventListener("click", function () {
   incrementIndex();
@@ -55,9 +68,72 @@ prev.onclick = function () {
   currentImg.setAttribute("src", images[currentIndex]);
 };
 
-document.addEventListener("keydown", function (e) {
-  if (e.key === ">") {
-    incrementIndex();
-    currentImg.setAttribute("src", images[currentIndex]);
+//functions to display and hide the navbar and cart
+navIcon.addEventListener("click", function () {
+  toggle(navigation);
+  overlay.classList.remove("hidden");
+});
+
+closeIcon.addEventListener("click", function () {
+  toggle(navigation);
+  overlay.classList.add("hidden");
+});
+
+cartIcon.addEventListener("click", () => toggle(cartDisplay));
+
+// document.addEventListener("keydown", function (e) {
+//   if (e.key === ">") {
+//     console.log(e.key);
+//     incrementIndex();
+//     currentImg.setAttribute("src", images[currentIndex]);
+//   }
+// });
+
+//functions to increment number of items to be added to the cart
+const incrementValue = function () {
+  var value = parseInt(document.getElementById("number").value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById("number").value = value;
+};
+
+const decrementValue = function () {
+  var value = parseInt(document.getElementById("number").value, 10);
+  if (value >= 1) {
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById("number").value = value;
+  }
+};
+
+plus.addEventListener("click", () => incrementValue());
+
+minus.addEventListener("click", () => decrementValue());
+
+numberBox.addEventListener("click", function () {
+  numberBox.value = "";
+});
+
+//Adding number of items to cart and displaying it in the cart
+const addToCartButton = document.querySelector(".add-to-cart");
+const cartNotification = document.querySelector(".cart-notification");
+const quantity = document.querySelector(".quantity");
+const finalPrice = document.querySelector(".cart__total-price-value");
+const emptyCartText = document.querySelector(".empty-cart-text");
+const updatedCartDetailsContainer = document.querySelector(".cart-items");
+
+const calcPrice = function () {
+  const priceToPay = numberBox.value * 125;
+  finalPrice.innerHTML = priceToPay;
+};
+
+addToCartButton.addEventListener("click", function () {
+  if (numberBox.value > 0) {
+    cartNotification.innerHTML = `${numberBox.value}`;
+    quantity.innerHTML = `${numberBox.value}`;
+    calcPrice();
+    emptyCartText.style.display = "none";
+    updatedCartDetailsContainer.style.display = "flex";
+    cartNotification.style.visibility = "visible";
   }
 });
